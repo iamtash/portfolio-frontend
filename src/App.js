8d63fe2db5feb91a9b45cd3d2fd075c9a9c98b80
iamtash <natashachernis@gmail.com>
2020-01-16 13:03:27 -0600

    initial setup of App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Dashboard from './components/Dashboard'
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
