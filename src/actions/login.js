3d6dacbce88ab25da0d7afd18a08cd23cd7d8013
iamtash <natashachernis@gmail.com>
2020-01-18 16:31:52 -0600

    make simple login action a thunk action

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,6 +1,22 @@
-export const login = (user) => {
-    return {
-        type: 'USER_LOGIN',
-        user
+import axios from "axios"
+
+const url = "http://localhost:3001/sessions"
+
+export const login = ({email, password}) => {
+    return dispatch => {
+        dispatch({type: 'LOGGING_IN'})
+        axios.post(url, {
+            user: { email, password }
+        }, {withCredentials: true }
+        ).then(resp => {
+            if (resp.data.logged_in) {
+                const { id, email } = resp.data.user
+                dispatch({
+                    type: 'USER_LOGIN', 
+                    user: { id, email }
+                }) 
+            }
+        }
+        ).catch(error => console.log('login error', error))
     }
 }
