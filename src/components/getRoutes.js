9bc8fe098b83afa4b3ce6fa45b2e759a3a38d69c
iamtash <natashachernis@gmail.com>
2020-01-20 11:40:58 -0600

    add and remove some routes

--- src/components/getRoutes.js
+++ src/components/getRoutes.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './Home'
-import DashboardContainer from '../containers/DashboardContainer'
 import ProjectInput from './ProjectInput'
 import BlogInput from './BlogInput'
+import About from './About'
+import ProjectsContainer from '../containers/ProjectsContainer'
+import GuestNav from './GuestNav'
 
-export const getRoutes = currentUser => {
+export const getRoutes = (currentUser, projects) => {
     return (
         <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/dashboard" component={DashboardContainer} />
-          <Route 
-            exact path="/new-project" 
-            render={() => currentUser.loggedIn ? <ProjectInput /> : <Home />} 
-          />
-          <Route 
-            exact path="/new-blog" 
-            render={() => currentUser.loggedIn ? <BlogInput /> : <Home />}
-          />
-        </Switch>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route 
+                    exact path="/new-project" 
+                    render={() => currentUser.loggedIn ? <ProjectInput /> : <Home />} 
+                />
+                <Route 
+                    exact path="/new-blog" 
+                    render={() => currentUser.loggedIn ? <BlogInput /> : <Home />}
+                />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/projects">
+                    {currentUser.loggedIn ? null : <GuestNav />}
+                    <ProjectsContainer projects={projects} />
+                </Route>
+            </Switch>
         </Router>
     )
 }
