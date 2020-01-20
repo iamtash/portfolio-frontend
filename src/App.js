4713aefc31b283f940a787a14e457bfc6f6f2f2d
iamtash <natashachernis@gmail.com>
2020-01-20 11:36:35 -0600

    load projects from db at highest level App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
 import { getRoutes } from './components/getRoutes'
 import { connect } from 'react-redux'
-
+import { getProjects } from './actions/projects'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getProjects()
+  }
+
   render() {
+    const { currentUser, projects } = this.props
     return (
       <div className="app">
-        {getRoutes(this.props.currentUser)}
+        {getRoutes(currentUser, projects)}
       </div>
     );
   }
@@ -15,8 +20,9 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    projects: state.projects
   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, { getProjects })(App);
