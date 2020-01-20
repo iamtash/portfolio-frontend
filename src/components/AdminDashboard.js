0a221ae183e27d70b04a4fa9982ced21e7354cdc
iamtash <natashachernis@gmail.com>
2020-01-20 11:40:34 -0600

    add ProjectsContainer to AdminDashboard, pass down projects prop

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
+import ProjectsContainer from '../containers/ProjectsContainer'
 
-const AdminDashboard = () => {
+const AdminDashboard = ({projects}) => {
     return (    
         <div>
             Admin Dashboard
-            <nav className="navbar navbar-expand-sm bg-light">
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <NavLink className="nav-link" to="/new-project">New Project</NavLink>
@@ -15,6 +16,7 @@ const AdminDashboard = () => {
                     </li>
                 </ul>
             </nav>
+            <ProjectsContainer projects={projects} />
         </div>
     )
 }
