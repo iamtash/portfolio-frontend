49013799bf4414985213eba10ffa70785c96a10e
iamtash <natashachernis@gmail.com>
2020-01-21 15:43:31 -0600

    add like, delete, and edit actions to projects

--- src/actions/projects.js
+++ src/actions/projects.js
@@ -23,12 +23,59 @@ export const getProjects = () => {
 export const addProject = newProject => {
     const project = humps.decamelizeKeys(newProject)
     return dispatch => {
-        // dispatch({type: 'REQUESTING_PROJECTS'})
         axios.post(url, { project })
             .then(resp => {
                 console.log(resp)
-                debugger
+                dispatch({
+                    type: 'ADD_PROJECT',
+                    project: humps.camelizeKeys(resp.data)
+                })
             })
             .catch(error => console.log('project posting error', error))
+    }
+}
+
+export const likeProject = id => {
+    return dispatch => {
+        axios.patch(`${url}/${id}`, { like: true })
+            .then(resp => {
+                console.log(resp)
+                const { id, likes } = resp.data
+                dispatch({
+                    type: 'LIKE_PROJECT',
+                    project: { id, likes }
+                })
+            })
+            .catch(error => console.log('project liking error', error))
+    }
+}
+
+export const deleteProject = id => {
+    return dispatch => {
+        axios.delete(`${url}/${id}`)
+            .then(resp => {
+                console.log(resp)
+                dispatch({
+                    type: 'DELETE_PROJECT',
+                    id: resp.data.id
+                })
+            })
+            .catch(error => console.log('project deleting error', error))
+    }
+}
+
+export const editProject = editedProject => {
+    const { id, title, githubUrl, demoUrl, description } = editedProject
+    const project = humps.decamelizeKeys({ title, githubUrl, demoUrl, description })
+    return dispatch => {
+        axios.patch(`${url}/${id}`, { project })
+            .then(resp => {
+                console.log(resp)
+                dispatch({
+                    type: 'EDIT_PROJECT',
+                    project: humps.camelizeKeys(resp.data)
+                })
+            })
+            .catch(error => console.log('project editing error', error))
     }
 }
