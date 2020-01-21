3a9cb25abdfe5fcebbf733626de71322ed5ff70d
iamtash <natashachernis@gmail.com>
2020-01-21 15:44:50 -0600

    simplify components and delete unnecessary ones

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import GuestNav from './GuestNav'
 import Avatar from "../media/avatar.png"
 
 const About = () => {
     return (
         <div>
-            <GuestNav />
             <div className="container">
                 <h1>About Me</h1>
                 <img src={Avatar} alt="avatar" className="rounded float-left" style={{width: '25%'}} />
