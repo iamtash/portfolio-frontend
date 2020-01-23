a0204bcf447a80505436d5cbd1d2730a2e8cdebe
iamtash <natashachernis@gmail.com>
2020-01-22 23:30:18 -0600

    control modal rendering

---
+++ src/containers/ModalFormContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import ModalForm from '../components/Modal'
+import { Button } from 'react-bootstrap'
+
+class ModalFormContainer extends Component {
+    state = {
+        showModal: false
+    }
+
+    handleShow = () => {
+        this.setState({
+            showModal: true
+        })
+    }
+
+    handleHide = () => {
+        this.setState({
+            showModal: false
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <Button variant="primary" block onClick={this.handleShow}>
+                    CREATE A NEW PROJECT
+                </Button>
+        
+                <ModalForm
+                    show={this.state.showModal}
+                    onHide={this.handleHide}
+                />
+            </div>
+            
+        )
+    }
+}
+
+export default ModalFormContainer
