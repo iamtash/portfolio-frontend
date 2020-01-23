b26c31b9e4d8f2b084e8da7d256945315ca4915c
iamtash <natashachernis@gmail.com>
2020-01-22 23:33:27 -0600

    add liking, deleting, and editing actions to projects reducer

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -17,10 +17,42 @@ export default function projectsReducer(
                     requesting: false
                 }
             case 'ADD_PROJECT':
+                console.log('we hit the reducer!!!!!')
                 return {
                     ...state,
                     projects: [...state.projects, action.project],
-                    requesting: false
+                    requesting: state.requesting
+                }
+            case 'LIKE_PROJECT':
+                return {
+                    ...state,
+                    requesting: state.requesting,
+                    projects: state.projects.map(project => {
+                        if (project.id === action.project.id) {
+                            return {
+                                ...project,
+                                likes: action.project.likes
+                            }
+                        } else return project
+                    })
+                }
+            case 'DELETE_PROJECT':
+                return {
+                    ...state,
+                    requesting: state.requesting,
+                    projects: state.projects.filter(project => {
+                        return project.id !== action.id
+                    })
+                }
+            case 'EDIT_PROJECT':
+                return {
+                    ...state,
+                    requesting: state.requesting,
+                    projects: state.projects.map(project => {
+                        if (project.id === action.project.id) {
+                            return action.project
+                        } else return project
+                    })
                 }
             default:
                 return state
