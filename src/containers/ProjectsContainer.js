f44bb3f049872fd4f563c1d216e24fc12da7b102
iamtash <natashachernis@gmail.com>
2020-01-22 23:32:34 -0600

    call new modal from container instead of basic form

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Projects from '../components/Projects'
-import ProjectInput from '../components/ProjectInput'
+import ModalFormContainer from './ModalFormContainer'
 
 class ProjectsContainer extends Component {
 
     render() {
         const { projects, currentUser } = this.props
         return(
-            <div className="container">
-                Projects Container
-                {currentUser.loggedIn ? <ProjectInput /> : null}
+            <div className="container" style={{marginTop: '20px'}}>
+                {currentUser.loggedIn ? <ModalFormContainer /> : null}
                 <Projects projects={projects} />
             </div>
         )
@@ -19,7 +18,8 @@ class ProjectsContainer extends Component {
 
 const mapStateToProps = state => {
     return {
-        currentUser: state.currentUser
+        currentUser: state.currentUser,
+        projects: state.projects
     }
 }
 
