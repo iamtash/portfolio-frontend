c5a09c48086d2b43f819a0067e13b7d04b4c46a5
iamtash <natashachernis@gmail.com>
2020-01-22 23:29:33 -0600

    add search / filter functionality

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,51 @@
-import React from 'react'
-import Project from './Project'
+import React, { Component } from 'react'
+import ProjectContainer from '../containers/ProjectContainer'
+import memoize from 'memoize-one'
 
-const Projects = ({projects}) => {
-    const renderProjects = projects => {
+class Projects extends Component {
+    state = {
+        filterText: ""
+    }
+
+    renderProjects = projects => {
         return (
-            <div className="container">
-                {projects.map(project => <Project key={project.id} {...project} />)}
+            <div>
+                {projects.map(project => <ProjectContainer key={project.id} project={project} />)}
             </div>
         )
 
     }
 
-    return (
-        <div className="container">
-            <h1>Projects</h1>
-            {projects.requesting ? 'Loading...' : null}
-            {renderProjects(projects.projects)}
-        </div>
-    )
+    handleChange = event => {
+        this.setState({ filterText: event.target.value })
+    }
+
+    filter = memoize(
+        (projects, filterText) => projects.filter(project => {
+            return project.title.toLowerCase().includes(filterText.toLowerCase()) || project.description.toLowerCase().includes(filterText.toLowerCase())
+        })
+    );
+
+    render() {
+        const { projects } = this.props
+        const filteredProjects = this.filter(projects.projects, this.state.filterText)
+        return (
+            <div style={{marginTop: '20px'}}>
+                <input 
+                    type="text" 
+                    placeholder="Search" 
+                    className="form-control-sm float-right"
+                    onChange={this.handleChange}
+                />
+
+                <h1>Projects</h1>
+                {projects.requesting ? <div className="spinner-border text-primary"></div> : null}
+                {this.renderProjects(filteredProjects)}
+
+            </div>
+        )
+    }
+
 }
 
 export default Projects
