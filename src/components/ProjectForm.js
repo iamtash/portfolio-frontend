39607ff3c4f5a7a190195b2afd80b7da8b19dc1d
iamtash <natashachernis@gmail.com>
2020-01-26 11:36:35 -0600

    add bootsrap styling

--- src/components/ProjectForm.js
+++ src/components/ProjectForm.js
@@ -1,44 +1,48 @@
 import React from 'react'
+import { Form } from 'react-bootstrap'
 
 const ProjectForm = ({ title, githubUrl, demoUrl, description, handleChange }) => {
     return (
-        <form className="form-group" style={{marginBottom: 0}}>
-            <input 
-                className="form-control"
-                onChange={handleChange}
-                type="text"
-                name="title"
-                value={title}
-                placeholder="Title"
-                required
-            />
-            <input 
-                className="form-control"
-                onChange={handleChange}
-                type="text"
-                name="githubUrl"
-                value={githubUrl}
-                placeholder="Github URL"
-                required
-            />
-            <input 
-                className="form-control"
-                onChange={handleChange}
-                type="text"
-                name="demoUrl"
-                value={demoUrl}
-                placeholder="Demo URL"
-                required
-            />
-            <textarea
-                className="form-control"
-                onChange={handleChange}
-                name="description"
-                value={description}
-                placeholder="Description"
-                required
-            />
-        </form>
+        <Form style={{marginBottom: 0}}>
+            <Form.Group>
+                <Form.Control 
+                    onChange={handleChange}
+                    type="text"
+                    name="title"
+                    value={title}
+                    placeholder="Title"
+                    required
+                    style={{marginBottom: '10px'}}
+                />
+                <Form.Control 
+                    onChange={handleChange}
+                    type="text"
+                    name="githubUrl"
+                    value={githubUrl}
+                    placeholder="Github URL"
+                    required
+                    style={{marginBottom: '10px'}}
+                />
+                <Form.Control 
+                    onChange={handleChange}
+                    type="text"
+                    name="demoUrl"
+                    value={demoUrl}
+                    placeholder="Demo URL"
+                    required
+                    style={{marginBottom: '10px'}}
+                />
+                <Form.Control
+                    as="textarea"
+                    onChange={handleChange}
+                    name="description"
+                    value={description}
+                    placeholder="Description"
+                    rows="4"
+                    required
+                />
+            </Form.Group>
+        </Form>
     )
 }
 
